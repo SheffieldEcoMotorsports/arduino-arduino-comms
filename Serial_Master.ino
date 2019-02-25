811a00018f380cd27b2247d2e38ef47ec4083d5f
Yadav Dosieah <44868771+YadavDosieah@users.noreply.github.com>
2019-02-25 18:08:23 +0000

    Serial_Master code update with the comms protocols

--- Serial_Master.ino
+++ Serial_Master.ino
@@ -11,66 +11,119 @@
 #define LAP_DISPLAY_DIO 6 //lap number display
 #define TIME_DISPLAY_DIO 7 //time display
 
-#define rxPin2 14
-#define txPin2 15
+
+//Arduino 2 uses normal serial & Arduino 3 uses software serial
+#define rxPin2 14 //Arduino 3 rx
+#define txPin2 15 //Arduino 3 tx
 #define ssPin 10 //SD Card 
-#define IntPin2 3//Interrupt 
-SoftwareSerial mySerial =  SoftwareSerial(rxPin2, txPin2); //Arduino B comms
+#define IntPin3 3//Interrupt for Arduino 3
+#define IntPin2 2//Interrupt for Arduino 2
+SoftwareSerial mySerial =  SoftwareSerial(rxPin2, txPin2); //Arduino 3 comms
 
 bool sdInitSuccess = false; //card init status
 File myFile;
-String Str1 = "";
 String Str2 = "";
+String Str3 = "";
 
-//TM1637Display speed_display(DISPLAY_CLK, SPEED_DISPLAY_DIO);  //set up the speed display
+TM1637Display speed_display(DISPLAY_CLK, SPEED_DISPLAY_DIO);  //set up the speed display
 TM1637Display batt_display(DISPLAY_CLK, BATT_DISPLAY_DIO);  //set up the battery display
-//TM1637Display lap_display(DISPLAY_CLK, LAP_DISPLAY_DIO);  //set up the lap display
-//TM1637Display time_display(DISPLAY_CLK, TIME_DISPLAY_DIO);  //set up the stopwatch display
+TM1637Display lap_display(DISPLAY_CLK, LAP_DISPLAY_DIO);  //set up the lap display
+TM1637Display time_display(DISPLAY_CLK, TIME_DISPLAY_DIO);  //set up the stopwatch display
 
 void setup()
 {
   batt_display.setBrightness(0x0f);
-  mySerial.begin(9600);
-  Serial.begin(9600);
+
+  // To arduino 2
+  Serial.begin(9600); 
   pinMode(IntPin2, OUTPUT);
   digitalWrite(IntPin2, LOW);
+
+  // To arduino 3
+  mySerial.begin(9600); 
+  pinMode(IntPin3, OUTPUT);
+  digitalWrite(IntPin3, LOW);
+  
   if(SD.begin(ssPin))
   {
     sdInitSuccess = true;
   }
+  
+  if (SD.exists("TEST.txt"))
+  {
+    SD.remove("TEST.txt");
+  }
+  myFile = SD.open("TEST.txt", FILE_WRITE);
+  myFile.println("Time,BatLvl,Speed,Lat,Log,AccX,AccY,AccZ,Pitch,Yaw,Roll,Current,Temp");
+  myFile.close();
+  
 }
 
 void loop()
 {
-  Str2 = "";
-  digitalWrite(IntPin2, HIGH);        // sets the digital pin 3 off
+
+  /// Arduino 3 //// 
+  Str3 = "";
+  digitalWrite(IntPin3, HIGH);        // sets the digital pin 3 off
   myFile = SD.open("TEST.txt", FILE_WRITE);
   
   while (mySerial.available())
  {
    char c = mySerial.read(); 
-   //Serial.print(c);
+   Str3.concat(c);
+ }
+  digitalWrite(IntPin3, LOW);        // sets the digital pin 13 off
+  delay(500);
+
+  int CommaIndex1 = Str3.indexOf(",");//Lat
+  int CommaIndex2 = Str3.indexOf(",",CommaIndex1+1);//Long
+  int CommaIndex3 = Str3.indexOf(",",CommaIndex2+1);//Speed
+  int CommaIndex4 = Str3.indexOf(",",CommaIndex3+1);//Acc1
+  int CommaIndex5 = Str3.indexOf(",",CommaIndex4+1);//Acc2
+  int CommaIndex6 = Str3.indexOf(",",CommaIndex5+1);//Acc3
+  int CommaIndex7 = Str3.indexOf(",",CommaIndex6+1);//Pos1 - Pitch
+  int CommaIndex8 = Str3.indexOf(",",CommaIndex7+1);//Pos2 - Yaw
+  //Pos3 - Roll
+
+  int Lat = (Str3.substring(1,CommaIndex1)).toInt();
+  int Long = (Str3.substring(CommaIndex1+1,CommaIndex2)).toInt();
+  int Speed = (Str3.substring(CommaIndex2+1,CommaIndex3)).toInt();
+  int AccX = (Str3.substring(CommaIndex3+1,CommaIndex4)).toInt();
+  int AccY = (Str3.substring(CommaIndex4+1,CommaIndex5)).toInt();
+  int AccZ = (Str3.substring(CommaIndex5+1,CommaIndex6)).toInt();
+  int Pitch = (Str3.substring(CommaIndex6+1,CommaIndex7)).toInt();
+  int Yaw = (Str3.substring(CommaIndex7+1,CommaIndex8)).toInt();
+  int Roll = (Str3.substring(CommaIndex8+1,Str2.length()-1)).toInt();
+  
+  speed_display.showNumberDec(Speed, false);
+  
+
+/// Arduino 2 //// 
+  Str2 = "";
+  digitalWrite(IntPin2, HIGH);        // sets the digital pin 2 high
+  
+  while (Serial.available())
+ {
+   char c = Serial.read(); 
    Str2.concat(c);
  }
-  digitalWrite(IntPin2, LOW);        // sets the digital pin 13 off
+  digitalWrite(IntPin2, LOW);        // sets the digital pin 2 low
   delay(500);
-  myFile.close();
 
-  int CommaIndex1 = Str2.indexOf(",");//Lat
-  int CommaIndex2 = Str2.indexOf(",",CommaIndex1+1);//Long
-  int CommaIndex3 = Str2.indexOf(",",CommaIndex2+1);//BatLvl
-  int CommaIndex4 = Str2.indexOf(",",CommaIndex3+1);//Acc1
-  int CommaIndex5 = Str2.indexOf(",",CommaIndex4+1);//Acc2
-  int CommaIndex6 = Str2.indexOf(",",CommaIndex5+1);//Acc3
-  //Acc4
-  
-  String BatLvl = Str2.substring(CommaIndex2+1,CommaIndex3);
-  int BattLvl = BatLvl.toInt();
-  Serial.print(BattLvl);
+  int CommaIndex1 = Str2.indexOf(",");//Current
+  int CommaIndex2 = Str2.indexOf(",",CommaIndex1+1);//Temp
+  //BatLvl
+
+  int Current = (Str2.substring(1,CommaIndex1)).toInt();
+  int Temp = (Str2.substring(CommaIndex1+1,CommaIndex2)).toInt();
+  int BatLvl = (Str2.substring(CommaIndex2+1,Str2.length()-1)).toInt();
   batt_display.showNumberDec(BattLvl, false);
 
-//    speed_display.setBrightness(0x0f);
-//  
-//  lap_display.setBrightness(0x0f);
-//  time_display.setBrightness(0x0f);
+
+  int Time = millis();
+  //SD Card Logging
+  //myFile.println("Time,BatLvl,Speed,Lat,Log,AccX,AccY,AccZ,Pitch,Yaw,Roll,Current,Temp");
+  myFile.println(Time+","+BatLvl+","+Speed+","+Lat+","+Log+","+AccX+","+AccY+","+AccZ+","+Pitch+","+Yaw+","+Roll+","+Current+","+Temp);
+
+  myFile.close();
 }
