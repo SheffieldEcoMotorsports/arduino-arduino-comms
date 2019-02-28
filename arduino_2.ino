a8a0d829fa4030f3758ad4c17ce4601b077f7337
mohammad-bahgat <45176729+mohammad-bahgat@users.noreply.github.com>
2019-02-28 18:29:50 +0000

    Update arduino_2.ino all variables are corrected, removed all Serial. on line 0 other than the interrupt routine

--- arduino_2.ino
+++ arduino_2.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 #include "Adafruit_MAX31855.h"
-
+#include <SoftwareSerial.h>
 // Pins for thermocouple
 #define CS 10
 #define MISO 12
@@ -8,16 +8,14 @@
 
 const byte interruptPin = 2;
 unsigned long time;
-String output;
-int current = 2;
+String output,out;
+//int current = 2;
 int val = 0;
 
+SoftwareSerial BTserial(5,6);
 Adafruit_MAX31855 Temp(SCLK, CS, MISO); // Creation of the thermocouple object
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("test");
-  pinMode(ledPin, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), acquisition, RISING);
 
@@ -29,7 +27,6 @@ void setup() {
   {
     while (BTserial.available() > 0) {
       char c = BTserial.read();
-      Serial.write(c);
     }
   }
   BTserial.print("AT+NAMECereal_bus");   //Set name (Should return msg "OKsetname")
@@ -38,7 +35,6 @@ void setup() {
   {
     while (BTserial.available() > 0) {
       char c = BTserial.read();
-      Serial.write(c);
     }
   }
   BTserial.print("AT+PIN5678");       //Set pin (Should return msg "OKsetPIN");
@@ -47,41 +43,36 @@ void setup() {
   {
     while (BTserial.available() > 0) {
       char c = BTserial.read();
-      Serial.write(c);
     }
   }
 }
 
 void loop() {
-  String out, curent, thermocouple, bluetooth;
-  current = current();
+  String  current, thermocouple, bluetooth;
+  current = Currentfcn();
   //thermistor=thermistor();
-  thermocouple = thermocouple();
-  bluetooth = bluetooth();
+  thermocouple = Thermocouplefcn();
+  bluetooth = Bluetoothfcn();
   out = "[" + current + "," + thermocouple + "," + bluetooth + "]";
-  Serial.println(out);
 }
 
-String bluetooth() {
+String Bluetoothfcn() {
   if (BTserial.available() > 0) { // triggered if something new is printed in bluetooth COM port
-    Str = "";
+   String Str = "";
     while (BTserial.available() > 0) {
       // read the newest byte in the serial buffer:
-      incomingControl = BTserial.read();
+     char incomingControl = BTserial.read();
       Str = Str + incomingControl;
     }
-    Serial.println(Str);
-    //Serial.println(Index1);
-    //Serial.println(incomingControl);
     //If serial reads 1, car moves backwards
     // *** PLEASE NOTE all of the motion commands are reversed due to the configuration of the remote and the set up of motors***
     //if (incomingControl == '1')
-    speed_display.showNumberDecEx(Str.toInt(), B01000000, false);
+    
   }
 }
 
 
-String current() {
+String Currentfcn() {
   /*
     float volt;
     int val = 0;
@@ -92,19 +83,19 @@ String current() {
     return(String(volt));
   */
   int dummy = random(0, 100);
-  return (String(dummy))
+  return (String(dummy));
 }
 
-String thermocouple() {
-  Temperature=Temp.readCelsius(); // Acquisition of temperature in degrees  Celsius
+String Thermocouplefcn() {
+  float  Temperature=Temp.readCelsius(); // Acquisition of temperature in degrees  Celsius
   delay(1000);
   return (String(Temperature));
 }
 
-void interruptFcn() {
-  mySerial.begin(9600);
+void acquisition() {
+  Serial.begin(9600);
   //state = !state;
-  mySerial.print(serialSend);
+  Serial.print(out);
   //delay(2);  //slow looping to allow buffer to fill with next character
-  mySerial.end();
+  Serial.end();
 }
